8890a8403b8fd56c6d0ac5289c03d263fa599aa5
PaulC98 <216263701+P-C-98@users.noreply.github.com>
2025-10-21 12:43:55 +0700

    added sound

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let time = 0;
 let countdownInterval;
 
+const counterSound = new Audio("./Sounds/timer.mp3");
 const countdownEl = document.getElementById('countdown');
 const inputEl = document.getElementById('time-input');
 const buttonEl = document.getElementById('my-button');
@@ -8,8 +9,7 @@ const buttonEl = document.getElementById('my-button');
 buttonEl.onclick = function () {
     const startingMinutes = parseFloat(inputEl.value);
     time = startingMinutes * 60;
-    console.log("Timer set for", startingMinutes, "minutes");
-    
+    console.log("Timer set for", startingMinutes, "minutes");  
 }
 
 setInterval(updateCountdown, 1000);
@@ -22,9 +22,22 @@ function updateCountdown () {
     seconds = seconds < 10 ? '0' + seconds : seconds;
 
     countdownEl.innerHTML = `${minutes}: ${seconds}`;
+
+    if (time == 30) {
+        counterSound.currentTime = 0;
+        counterSound.play();
+    }
+
+    if (time === 1) {
+        counterSound.pause();
+        counterSound.currentTime = 0;
+    }
+
     time--;
 
     }else{
+        clearInterval(countdownInterval);
         countdownEl.innerHTML = "0:00";
     }
 }
+
